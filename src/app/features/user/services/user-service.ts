3f1552d0b6209ea87a79dec497a0bf36ee17bd14
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-09-17 02:02:28 -0600

    Add UserService

--- src/app/features/user/services/user-service.ts
+++ src/app/features/user/services/user-service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { RegisterUserDto } from '../models/register-user-dto';
+import { Observable } from 'rxjs';
+import { LoginUserDto } from '../models/login-user-dto';
+import { LoginResponse } from '../models/login-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+
+  protected basePath = 'http://localhost:8081';
+  public headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private httpClient: HttpClient) {}
+
+  public login(body: LoginUserDto): Observable<LoginResponse> {
+    return this.httpClient.post<LoginResponse>(`${this.basePath}/auth/login`, body, {headers: this.headers});
+  }
+
 }

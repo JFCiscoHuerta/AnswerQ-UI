86a72e90ff52f9e9e4e6fe0e6f8a683eed501ef8
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-09-19 02:14:43 -0600

    Add RegisterUserResponse Interface

---
+++ src/app/features/user/models/register-user-response.ts
@@ -0,0 +1,13 @@
+export interface RegisterUserResponse {
+  id: number,
+  username: string,
+  firstname: string,
+  lastname: string,
+  birthdate: string,
+  email: string,
+  gender: string,
+  phoneNumber: string,
+  enabled: boolean,
+  forms: [],
+  userAnswers: []
+}

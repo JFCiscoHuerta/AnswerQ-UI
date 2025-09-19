f683c303412efbdf33b2b6b40228de5d0a278e2e
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-09-19 02:27:03 -0600

    Add logout Feature in header.ts File

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../../features/user/services/user-service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+
+  public router = inject(Router);
+  public userService = inject(UserService);
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/signin'])
+  }
 
 }

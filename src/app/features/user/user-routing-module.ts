bd702cde499f8327871109e175df0ef731289cda
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-09-22 16:25:47 -0600

    Add VerifyAccount path

--- src/app/features/user/user-routing-module.ts
+++ src/app/features/user/user-routing-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignIn } from './components/sign-in/sign-in';
 import { SignUp } from './components/sign-up/sign-up';
+import { VerifyAccount } from './components/verify-account/verify-account';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignIn },
-  { path: 'sign-up', component: SignUp }
+  { path: 'sign-up', component: SignUp },
+  { path: 'verify-account', component: VerifyAccount }
 ]
 
 @NgModule({

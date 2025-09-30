7b049b41400828bc492d7d0c626c1883d9cc8efb
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-09-29 18:36:19 -0600

    Enhance Header Design & Functionality

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -2,12 +2,22 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../features/auth/services/auth-service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthRoutingModule } from "../../../features/auth/auth-routing-module";
 
 @Component({
   selector: 'app-header',
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    AuthRoutingModule
+],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
@@ -18,7 +28,7 @@ export class Header {
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['/sign-in'])
+    this.router.navigate(['/auth/sign-in'])
   }
 
 }
